798e4eba57a6995a01009210de377cf587be6e19
Sanaya James <sanaya.j.james@gmail.com>
2026-04-21 18:24:20 -0700

    added relationships in models. models test passed

--- server/models.py
+++ server/models.py
@@ -9,7 +9,11 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 # TODO: add association table
-
+sessionspeaker = db.Table(
+    'sessionspeaker',
+    db.Column('session_id', db.Integer, db.ForeignKey('sessions.id'), primary_key=True),
+    db.Column('speaker_id', db.Integer, db.ForeignKey('speakers.id'), primary_key=True)
+)
 
 # TODO: set up relationships for all models
 class Event(db.Model):
@@ -18,6 +22,8 @@ class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     location = db.Column(db.String, nullable=False)
+
+    sessions = db.relationship('Session', back_populates='event', cascade='all, delete-orphan')
 
     def __repr__(self):
         return f'<Event {self.id}, {self.name}, {self.location}>'
@@ -28,8 +34,12 @@ class Session(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     start_time = db.Column(db.DateTime)
-    event_id = db.Column(db.Integer)
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
 
+    event = db.relationship('Event', back_populates='sessions')
+    speakers = db.relationship('Speaker', secondary=sessionspeaker, back_populates='sessions')
+
+    # speakers = association_proxy('speakers', 'sessions',)
 
     def __repr__(self):
         return f'<Session {self.id}, {self.title}, {self.start_time}>'
@@ -41,15 +51,22 @@ class Speaker(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
 
+    sessions = db.relationship('Session', secondary=sessionspeaker, back_populates='speakers')
+    bio = db.relationship('Bio', uselist=False, back_populates='speaker')
+
+    # sessions = association_proxy('speakers', 'sessions',)
+
     def __repr__(self):
-        return f'<Speaker {id}, {name}>'
+        return f'<Speaker {self.id}, {self.name}>'
 
 class Bio(db.Model):
     __tablename__ = 'bios'
 
     id = db.Column(db.Integer, primary_key=True)
     bio_text = db.Column(db.Text, nullable=False)
-    speaker_id = db.Column(db.Integer)
+    speaker_id = db.Column(db.Integer, db.ForeignKey('speakers.id'))
+
+    speaker = db.relationship('Speaker', back_populates='bio')
 
     def __repr__(self):
-        return f'<Bio {id}, {bio_text}>'
+        return f'<Bio {self.id}, {self.bio_text}>'
